d6a5b758027a22ca9678c85ccb9f4ceaec3e7189
ertdfgcb <odaypaul@gmail.com>
2014-02-03 15:18:50 -0800

    removed log and edit, they were superflous

--- tools/log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### log.sh file
-### Asks the user for a line, adds that line with timestamp to file
-
-now=$(date +"%D %_I:%M %p")
-log_message="Write line for $now to $1"
-log_line=$($tools/menu.sh -noinput -y 200 -p "$log_message")
-echo "$now: $log_line" >> ~/log
