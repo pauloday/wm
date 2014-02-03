546c4eedf9558e788e9009c1f73740a2de274903
ertdfgcb <odaypaul@gmail.com>
2014-02-02 20:58:01 -0800

    tray.sh now uses trayer

--- tools/tray.sh
+++ tools/tray.sh
@@ -4,11 +4,11 @@
 
 source $wm/style.sh
 
-pid_file="/tmp/stalonetray_pid"
+pid_file="/tmp/trayer_pid"
 if [ -f $pid_file ]; then
 	kill -9 $(cat $pid_file)
 	rm -rf $pid_file
 	exit
 fi
-stalonetray -bg "${colors[bg]}" &
+trayer --edge top --widthtype request --heighttype request --expand true &
 echo $! > $pid_file
