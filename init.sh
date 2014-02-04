fde7984c99c788937495f19c1cabea52ee0375eb
ertdfgcb <odaypaul@gmail.com>
2014-02-03 21:22:10 -0800

    new colors, fixed wierd init issue

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ export wm="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export tools="$wm/tools"
 setxkbmap -option ctrl:nocaps
 xsetroot -cursor_name left_ptr
-xrdb -load $wm/.xdefaults
+xrdb -load $wm/xdefaults
 compton &
 sxhkd -c $wm/sxhkdrc &
 bspwm -c $wm/bspwmrc
