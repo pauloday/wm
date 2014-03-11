790b2ffad7c5f5deccb0a262cacd0e8792ba6f6a
ertdfgcb <odaypaul@gmail.com>
2014-03-10 23:04:15 -0700

    modified to define each color as a variable

--- style.sh
+++ style.sh
@@ -12,17 +12,33 @@ invert_colors() {
 		 ^fg(${colors[fg]})^bg(${colors[bg]})"
  }
 
-color_vals=( $(xrdb -query | grep -P "color[0-9]*" | sort | cut -f 2-) )
-count=0
-declare -A colors
-for name in black brightgreen brightyellow brightblue brightmagenta brightcyan brightwhite red green yellow blue magenta cyan white grey brightred; do
-	#echo "pre; count: $count color_vals[count]: ${color_vals[$count]} name: ${name}"
-	colors[${name}]=${color_vals[$count]}
-	#echo "post; colors[name]: ${colors[${name}]}"
+colors=( $(xrdb -query | grep -P "color[0-9]*" | sort | cut -f 2-) )
+color_names=(
+	black 
+	brightgreen 
+	brightyellow
+	brightblue
+	brightmagenta
+	brightcyan
+	brightwhite
+	red
+	green
+	yellow
+	blue
+	magenta
+	cyan
+	white
+	grey
+	brightred )
+count=1
+
+for name in $color_names; do
+	eval "$name=${colors[$count]}"
 	((count++))
 done
-colors[fg]=$(xrdb -query | grep -P "dzen2[\.\*]foreground:" | awk '{print $NF}')
-colors[bg]=$(xrdb -query | grep -P "dzen2[\.\*]background:" | awk '{print $NF}')
+
+fg=$(xrdb -query | grep -P "dzen2[\.\*]foreground:" | awk '{print $NF}')
+bg=$(xrdb -query | grep -P "dzen2[\.\*]background:" | awk '{print $NF}')
 
 font=$(xrdb -query | grep -P "dzen2[\*\.]font:" | sed 's/dzen2[\*\.]font:\(.*\)/\1/' | tr -d "[:blank:]")
 
